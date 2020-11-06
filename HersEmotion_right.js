57361b9e6214de2239bf5dcc9c98556bb5f0be99
yann en chemin <73861577+yannenchemin@users.noreply.github.com>
2020-11-07 04:22:15 +0900

    Revert "주석 삭제"
    This reverts commit 999dd9e8c6c063195d0db100b210cd833d65baaa.

--- HersEmotion_right.js
+++ HersEmotion_right.js
@@ -18,10 +18,10 @@ EmotionGirl.prototype.run = function() {
 };
 //-------------------------------------------------------------------------
 let detail_of_mind_girl = function(position) {
-  this.acceleration = createVector(1, -0.1);
-  this.share = createVector(random(-30, -35), random(-1, 6));
+  this.acceleration = createVector(1, -0.1); //뿜어져나오는 힘,뿜어져나오는 각도
+  this.share = createVector(random(-30, -35), random(-1, 6)); //1번 파라미터는 좌우로 퍼지는 간격, 2번은 뿜어져나오는 통로굵기
   this.position = position.copy();
-  this.Exterior_or_insensitivity = 255;
+  this.Exterior_or_insensitivity = 255; // 숫자가 적어질 수록 전체적으로 파티클이 흐려짐
 };
 //-------------------------------------------------------------------------
 detail_of_mind_girl.prototype.run = function() {
@@ -32,7 +32,7 @@ detail_of_mind_girl.prototype.run = function() {
 detail_of_mind_girl.prototype.move = function() {
   this.share.add(this.acceleration);
   this.position.add(this.share);
-  this.Exterior_or_insensitivity -= 2
+  this.Exterior_or_insensitivity -= 2 
 
 };
 //-------------------------------------------------------------------------
@@ -46,5 +46,5 @@ detail_of_mind_girl.prototype.ShowMe = function() {
 //-------------------------------------------------------------------------
 
 detail_of_mind_girl.prototype.isfeel = function() {
-  return this.Exterior_or_insensitivity/*?*/ < 0  
+  return this.Exterior_or_insensitivity < 0  //외면 혹은 무감각
 };
