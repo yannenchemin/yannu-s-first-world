4998ebf266ef8ed75a3d9f71ea07973c5f747cfe
yann en chemin <73861577+yannenchemin@users.noreply.github.com>
2020-11-08 20:22:29 +0900

    완성!!

--- Emotional_Move_js/HesEmotion_left.js
+++ Emotional_Move_js/HesEmotion_left.js
@@ -18,11 +18,11 @@ EmotionBoy.prototype.run = function() {
 };
 //------------------------------------------------------------------------
 
-let detail_of_mind_boy = function(position) {
-  this.acceleration = createVector(-0.1, 0.05); //가속도: 뿜어져나오는 방향,뿜어져나오는 각도
-  this.share = createVector(random(10, 15), random(-5, 1)); //속도: 1번 파라미터는  퍼지는 파티클 거리(속도), 2번은 뿜어져나오는 통로굵기
+let detail_of_mind_boy = function(position) { // 나의 생각
+  this.acceleration = createVector(-0.1, 0.05);
+  this.share = createVector(random(10, 15), random(-5, 1));
   this.position = position.copy();
-  this.unrequited_love = 255; // 숫자가 적어질 수록 전체적으로 파티클이 흐려짐
+  this.unrequited_love = 255;
 };
 //-------------------------------------------------------------------------
 detail_of_mind_boy.prototype.run = function() {
@@ -32,10 +32,9 @@ detail_of_mind_boy.prototype.run = function() {
 //-------------------------------------------------------------------------
 
 detail_of_mind_boy.prototype.move = function() {
-  this.share.add(this.acceleration); //속도에 가속도를 더함
-  this.position.add(this.share); // 포지션에 속도를 더함
-  this.unrequited_love -= 2; //숫자가 올라갈 수록 튀어 올라온 파티클이 빠르게 흐려짐
-
+  this.share.add(this.acceleration);
+  this.position.add(this.share);
+  this.unrequited_love -= 2;
 };
 //-------------------------------------------------------------------------
 
